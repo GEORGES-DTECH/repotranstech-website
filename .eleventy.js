5e5027ebf045361024fe5803df4d2c4939bb2772
georges-dtech <georgendungu2671991@gmail.com>
2025-07-12 11:11:05 +0300

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const {DateTime} = require("luxon")
+module.exports = function(eleventyConfig) {
+   eleventyConfig.addPassthroughCopy('./src/styles.css');
+   eleventyConfig.addPassthroughCopy('./src/images');
+   eleventyConfig.addFilter("postdate",(dateObj)=>{
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+   })
+  return {
+     pathPrefix: "/repotranstech-website/",
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "public"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
