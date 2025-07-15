74e607742a68f101f0c7f3ee1fa90b18e33bb9b2
georges-dtech <georgendungu2671991@gmail.com>
2025-07-15 15:49:21 +0300

    added site map

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,22 @@
+const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 const {DateTime} = require("luxon")
+
 module.exports = function(eleventyConfig) {
    eleventyConfig.addPassthroughCopy('./src/styles.css');
+   eleventyConfig.addPassthroughCopy('./src/robots.txt');
    eleventyConfig.addPassthroughCopy('./src/images');
+   eleventyConfig.addPlugin(pluginSitemap, {
+    sitemap: {
+      hostname: "https://repotranstech.com",
+      changefreq: "daily",               
+      lastmod: true                      
+    },
+  });
    eleventyConfig.addFilter("postdate",(dateObj)=>{
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+   
+   
    })
   return {
    
